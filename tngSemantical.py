a11aff92cd74bfbfee42b1c451939969a6a7c4a7
Gabriel Dias <gabriellimadias2001@gmail.com>
2023-04-01 11:35:13 -0300

    addedGItignore

---
+++ tngSemantical.py
@@ -0,0 +1,106 @@
+from antlr4 import *
+from tngVisitor import tngVisitor
+
+if __name__ is not None and "." in __name__:
+    from .tngParser import tngParser
+else:
+    from tngParser import tngParser
+
+class tngSemantical(tngVisitor):
+    
+    def __init__(self):
+        self.symbolTable = {}
+
+        # { cadeia, token, cat, tipo, val}
+
+    # Visit a parse tree produced by tngParser#expression.
+    def visitExpression(self, ctx:tngParser.ExpressionContext):        
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by tngParser#atribuicao.
+    def visitAtribuicao(self, ctx:tngParser.AtribuicaoContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by tngParser#declaracao.
+    def visitDeclaracao(self, ctx:tngParser.DeclaracaoContext):
+        obj = { 'cadeia' : '', 'token' : '', 'cat' : '', 'tipo' : '', 'val' : '' }
+
+        print(self.visitChildren(ctx))
+
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by tngParser#chamada_print.
+    def visitChamada_print(self, ctx:tngParser.Chamada_printContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by tngParser#term.
+    def visitTerm(self, ctx:tngParser.TermContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by tngParser#factor.
+    def visitFactor(self, ctx:tngParser.FactorContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by tngParser#number.
+    def visitNumber(self, ctx:tngParser.NumberContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by tngParser#tipo.
+    def visitTipo(self, ctx:tngParser.TipoContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by tngParser#comando.
+    def visitComando(self, ctx:tngParser.ComandoContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by tngParser#inicio.
+    def visitInicio(self, ctx:tngParser.InicioContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by tngParser#expr_bool.
+    def visitExpr_bool(self, ctx:tngParser.Expr_boolContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by tngParser#fator_bool.
+    def visitFator_bool(self, ctx:tngParser.Fator_boolContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by tngParser#cond.
+    def visitCond(self, ctx:tngParser.CondContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by tngParser#if_bloco.
+    def visitIf_bloco(self, ctx:tngParser.If_blocoContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by tngParser#else_bloco.
+    def visitElse_bloco(self, ctx:tngParser.Else_blocoContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by tngParser#for_bloco.
+    def visitFor_bloco(self, ctx:tngParser.For_blocoContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by tngParser#while_bloco.
+    def visitWhile_bloco(self, ctx:tngParser.While_blocoContext):
+        return self.visitChildren(ctx)
+
+
+
+del tngParser
